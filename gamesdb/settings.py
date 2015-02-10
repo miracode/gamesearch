8287e924f8261de5f08673ef534a7d95a134c3d3
miracode <rascatim@gmail.com>
2015-02-10 15:10:17 -0800

    removed secrets

--- gamesdb/settings.py
+++ gamesdb/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'hnpyr6xjvhly_v5#&gqe@&#$@e4&d%0r=mnz!n&kja5c#4w7+_'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
