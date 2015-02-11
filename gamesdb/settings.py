c4a5c7f71dbeb50692b615a44b773eec6fc1b751
miracode <rascatim@gmail.com>
2015-02-10 16:27:57 -0800

    heroku set up

--- gamesdb/settings.py
+++ gamesdb/settings.py
@@ -24,7 +24,10 @@ DEBUG = True
 
 TEMPLATE_DEBUG = True
 
-ALLOWED_HOSTS = []
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
